93f9a22a6372a163db2ae09a57bdc14ba3df7556
Ezequias Lopes <63757378+ezequiaslopesdasilva@users.noreply.github.com>
2022-07-19 01:28:12 +0000

    feat: adicionar servidor express como ping_receiver

--- index.cjs
+++ index.cjs
@@ -1,7 +1,11 @@
 const Discord = require('discord.js');
+const Express = require('express');
 const Dotenv = require('dotenv');
 const Glob = require('glob');
+const Http = require('http');
 
+const express_app = Express();
+const http_server = Http.createServer(express_app);
 const client = new Discord.Client({
   intents: 32767,
   partials: ['MESSAGE', 'REACTION'],
@@ -24,4 +28,15 @@ Glob.sync('./events/**.cjs').forEach(event_path => {
 
 client.login(process.env.DISCORD_TOKEN).then(() => {
   console.log('[DISCORD] Client conectado')
+});
+
+express_app.get('/*', (req, res) => {
+  res.json({ 
+    status: 200,
+    message: 'OK'
+  });
+});
+
+http_server.listen(process.env.PORT, () => {
+  console.log(`[HTTP] Servidor conectado na porta ${process.env.PORT}`);
 });
