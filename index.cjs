d5cb9396c228dd1b55489e077a850d96d8f73f4b
nottja1mmm <nottja1mmm@gmail.com>
2022-07-21 11:21:42 -0300

    fix intents number and imports

--- index.cjs
+++ index.cjs
@@ -1,27 +1,24 @@
-const Discord = require('discord.js');
 const Express = require('express');
-const Dotenv = require('dotenv');
-const Glob = require('glob');
-const Http = require('http');
+const {sync} = require('glob');
+const {createServer} = require('http');
+const {config} = require('dotenv');
+const {Client} = require('discord.js');
 
 const express_app = Express();
-const http_server = Http.createServer(express_app);
-const client = new Discord.Client({
-  intents: 32767,
-  partials: ['MESSAGE', 'REACTION'],
-});
+const http_server = createServer(express_app);
+const client = new Client({intents: 79615});
 
-Dotenv.config();
+config();
 client.commands = new Map();
 
-Glob.sync('./commands/**/**.cjs').forEach(command_path => {
+sync('./commands/**/**.cjs').forEach(command_path => {
   var command = require(command_path);
   command.names.forEach(name => {
     client.commands.set(name, command);
   });
 });
 
-Glob.sync('./events/**/**.cjs').forEach(event_path => {
+sync('./events/**/**.cjs').forEach(event_path => {
   var event = require(event_path);
   event.run(client);
 });
