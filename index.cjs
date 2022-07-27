117a56931c247940c5648b4879bf802651491839
Ezequias Lopes <63757378+ezequiaslopesdasilva@users.noreply.github.com>
2022-07-27 17:07:05 +0000

    fix: add the paths to slash commands

--- index.cjs
+++ index.cjs
@@ -1,20 +1,28 @@
 const Express = require('express');
-const {sync} = require('glob');
-const {createServer} = require('http');
-const {config} = require('dotenv');
-const {Client} = require('discord.js');
+const { sync } = require('glob');
+const { createServer } = require('http');
+const { config } = require('dotenv');
+const { Client, Routes } = require('discord.js');
 
 const express_app = Express();
 const http_server = createServer(express_app);
-const client = new Client({intents: 112383});
+const client = new Client({ intents: 112383 });
 
 config();
 client.commands = new Map();
+client.slashs = new Map();
 
-sync('./commands/**/**.cjs').forEach(command_path => {
+sync('./commands/message/**/**.cjs').forEach(command_path => {
   var command = require(command_path);
   command.names.forEach(name => {
     client.commands.set(name, command);
+  });
+});
+
+sync('./commands/slash/**/**.cjs').forEach(command_path => {
+  var command = require(command_path);
+  command.names.forEach(name => {
+    client.slashs.set(name, command);
   });
 });
 
@@ -23,15 +31,13 @@ sync('./events/**/**.cjs').forEach(event_path => {
   event.run(client);
 });
 
+
 client.login(process.env.DISCORD_TOKEN).then(() => {
   console.log('[DISCORD] Client conectado')
 });
 
 express_app.get('/*', (req, res) => {
-  res.json({ 
-    status: 200,
-    message: 'OK'
-  });
+  res.json({ status: 200, message: 'OK' });
 });
 
 http_server.listen(process.env.PORT, () => {
