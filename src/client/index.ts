5d9326ae909db53a0617186fd1733bcac69254ef
DablioZe <wyskzito@gmail.com>
2022-12-01 06:10:14 -0300

    feat: create new client with modified core

---
+++ src/client/index.ts
@@ -0,0 +1,34 @@
+import { Client, Collection } from 'discord.js';
+import { Command } from 'interfaces';
+
+import settings from 'settings';
+import glob from 'glob';
+import path from 'path';
+
+export default class ExtendedClient extends Client {
+  public commands: Collection<string, Command> = new Collection();
+  public settings = settings;
+
+  public async init() {
+    this.login(this.settings['token']);
+
+    glob.sync('**/commands/**/**.public.ts')
+      .forEach(commandPath => {
+        const { default: command } = require(path
+          .join(__dirname, '../../', commandPath),
+        );
+        this.commands.set(command.data.name, command);
+      });
+
+    glob.sync('**/events/**/**.public.ts')
+      .forEach(eventPath => {
+        const { default: event } = require(path
+          .join(__dirname, '../../', eventPath),
+        );
+
+        this.on(event.name, (...args) => {
+          event.run(this, ...args);
+        });
+      });
+  };
+}
