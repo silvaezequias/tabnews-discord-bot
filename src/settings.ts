3c6982493cebf3c15279716272f60556f0df9c99
DablioZe <wyskzito@gmail.com>
2022-12-01 06:09:21 -0300

    feat: add global application settings

---
+++ src/settings.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+  token: process.env.TOKEN,
+  prefix: '!',
+  default: {
+    guildId: '997643740581736530',
+  },
+  log: {
+    enabled: true,
+    error: {
+      channelId: '1046245514930303067'
+    },
+    member: {
+      joined: {
+        channelId: '998713878861054052'
+      },
+      left: {
+        channelId: '998713878861054052'
+      }
+    }
+  }
+}; 
