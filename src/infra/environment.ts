54e05f512b4e9208184461c6292f42ed18725968
DablioZe <wyskzito@gmail.com>
2022-12-01 06:12:10 -0300

    feat: add environment checker

---
+++ src/infra/environment.ts
@@ -0,0 +1,12 @@
+export function isProduction(): boolean {
+  return !(process.env.NODE_ENV && process.env.NODE_ENV != 'development');
+};
+
+export default {
+  isProduction,
+  get type() {
+    return isProduction()
+      ? 'production'
+      : 'development'
+  }
+}
