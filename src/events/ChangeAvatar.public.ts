97e1721bbf33b2dfe4e9eb40b7455ddee9f95c5c
DablioZe <wyskzito@gmail.com>
2022-12-01 06:23:24 -0300

    feat: periodically change application avatar

---
+++ src/events/ChangeAvatar.public.ts
@@ -0,0 +1,33 @@
+import { Event } from 'interfaces';
+import { readFileSync, PathLike } from 'fs';
+import environment from 'infra/environment';
+
+export default {
+  name: 'ready',
+  run: (client) => {
+    if (!environment.isProduction()) return;
+
+    const avatarPaths: PathLike[] = [
+      '../assets/angry-face.png',
+      '../assets/dead-face.png',
+      '../assets/distrusting-face.png',
+      '../assets/love-face.png',
+      '../assets/normal-face.png',
+      '../assets/sleepy-face.png',
+      '../assets/unamused-face.png',
+      '../assets/woozy-face.png',
+    ];
+
+    async function setNewAvatar(path: PathLike) {
+      const avatarBuff = readFileSync(path);
+      await client.user.setAvatar(avatarBuff);
+    };
+
+    setInterval(async function () {
+      var randomNumber = Math.round(Math.random() * avatarPaths.length);
+      var randomAvatarPath = avatarPaths[randomNumber];
+
+      await setNewAvatar(randomAvatarPath);
+    }, ((60 * 1000) * 60) * 2); // A cada 2 horas
+  }
+} as Event
