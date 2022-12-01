b0b0fd360fd63d5e8fe7f03fe10bc41e0e95bf5b
DablioZe <wyskzito@gmail.com>
2022-12-01 06:30:26 -0300

    fix: environment check change

--- src/infra/environment.ts
+++ src/infra/environment.ts
@@ -1,5 +1,5 @@
 export function isProduction(): boolean {
-  return !(process.env.NODE_ENV && process.env.NODE_ENV != 'development');
+  return (process.env.NODE_ENV && process.env.NODE_ENV != 'development');
 };
 
 export default {
