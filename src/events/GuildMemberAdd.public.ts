ea762c241641b70c1dd6579462ff25d2a9703744
DablioZe <wyskzito@gmail.com>
2022-12-09 00:26:52 -0300

    fix: change model variable names to camelCase

--- src/events/GuildMemberAdd.public.ts
+++ src/events/GuildMemberAdd.public.ts
@@ -5,21 +5,21 @@ import ExtendedClient from "client";
 export default {
   name: 'guildMemberAdd',
   run: (client: ExtendedClient, member: GuildMember) => {
-    const message_embed = new EmbedBuilder()
+    const messageEmbed = new EmbedBuilder()
       .setTitle('Boas Vindas!!')
       .setDescription(`Olá **${member.user.username}**, seja bem-vindo(a). Estamos contentes por você ter se juntado à nós.`)
       .setImage('https://i.imgur.com/UnzjJG1.png')
       .setTimestamp(new Date())
       .setColor('#2DCF46');
 
-    const log_channel = client.channels.cache.get(
+    const logChannel = client.channels.cache.get(
       client.settings.log.member.joined.channelId
     );
 
-    if (log_channel.isTextBased()) {
-      (<TextChannel>log_channel)?.send({
+    if (logChannel.isTextBased()) {
+      (<TextChannel>logChannel)?.send({
         content: member.user.toString(),
-        embeds: [message_embed]
+        embeds: [messageEmbed]
       });
     }
   }
--- src/events/GuildMemberRemove.public.ts
+++ src/events/GuildMemberRemove.public.ts
@@ -5,19 +5,21 @@ import ExtendedClient from "client";
 export default {
   name: 'guildMemberRemove',
   run: (client: ExtendedClient, member: GuildMember) => {
-    const message_embed = new EmbedBuilder()
+    const messageEmbed = new EmbedBuilder()
       .setTitle('Até Mais!!')
       .setDescription(`Tchau **${member.user.username}** :cry: Ficamos tristes que tenha realmente que sair!! Esperamos que volte algum dia.`)
       .setImage('https://i.imgur.com/B0LQpbA.png')
       .setTimestamp(new Date())
       .setColor('#bf3c37');
 
-    const log_channel = client.channels.cache.get(
+    const logChannel = client.channels.cache.get(
       client.settings.log.member.left.channelId
     );
 
-    (<TextChannel>log_channel)?.send?.({
-      embeds: [message_embed]
-    });
+    if (logChannel.isTextBased()) {
+      (<TextChannel>logChannel)?.send?.({
+        embeds: [messageEmbed]
+      });
+    }
   }
 } as Event
